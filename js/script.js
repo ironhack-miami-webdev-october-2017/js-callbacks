b008921b5bc637eee810c20d533d55bd82512d20
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-10-25 12:13:22 -0400

    Add setInterval examples

--- js/script.js
+++ js/script.js
@@ -36,4 +36,56 @@ stopButton.onclick = function () {
     // append it to the container
     var container = document.querySelector('.container');
     container.appendChild(myP);
+}; // end onclick
+
+
+
+
+// Countdown code --------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+var countdownBtn = document.querySelector('.countdown-button');
+var intervalId;
+
+countdownBtn.onclick = function () {
+    // if there was a timer already started, stop it before making the new one.
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+
+    var countdownInput = document.querySelector('input');
+    // "value" only works on input tags
+    var countdownNumber = countdownNumber.value;
+
+    var countFeedback = document.querySelector('.count');
+    // update DOM with initial count
+    countFeedback.innerHTML = countdownNumber;
+
+    // "setInterval()" returns the interval ID (for canceling the interval)
+    intervalId =
+      // setInterval(CALLBACK, MILLISECONDS)
+      //   - calls the function over and over every time the amount of time passes
+      setInterval(
+        // this anonymous function is our callback
+        function () {
+            // decrease the count
+            countdownNumber -= 1;
+            // update DOM with updated count
+            countFeedback.innerHTML = countdownNumber;
+
+            // stop the "setInterval()" loop when the counter reaches 0
+            if (countdownNumber === 0) {
+                clearInterval(intervalId);
+            }
+        },
+        1000
+      );
+};
+
+
+var countStopBtn = document.querySelector('.count-stop');
+
+countStopBtn.onclick = function () {
+    // stop the "setInterval()" loop
+    clearInterval(intervalId);
 };
